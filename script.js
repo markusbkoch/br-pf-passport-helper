68891792da4e2f06a744f028c537592ff520ece1
Markus Buhatem Koch <34865315+markusbkoch@users.noreply.github.com>
2019-06-06 11:31:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+//altere os valores abaixo (CPF, número do protocolo do agendamento e data de nascimento)
+document.realizarAgendamentoForm.cpf.value = '000.000.001-91';
+document.realizarAgendamentoForm.protocolo.value = '1.2019.0000000000';
+document.realizarAgendamentoForm.dataNascimento.value = '01/01/1900';
+void(0)
